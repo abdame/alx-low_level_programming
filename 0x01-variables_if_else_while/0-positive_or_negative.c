8370b081557bf64aa1e7554a926b5a4327f8eb93
abdame <abdiwakamente2@gmail.com>
2022-04-11 13:03:34 -0700

    my first task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,22 +2,37 @@
 #include <time.h>
 #include <stdio.h>
 
-/*main-prints a rondom number
- * it is negative, pos, zero
- * return always0
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (succeses)
  */
-int main (void)
-{
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 
-	if (n > 1)
-		printf("%d is positive\n' ,n");
-	else if (n < 0)
-		printf("%d is negative\n, n");
-	else
-		printf("%d is zero\n", n);
-	return (0);
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n==0)
+{
+printf("%i is zero\n", n);
+}
+
+else if (n<0)
+{
+printf("%i is negative\n", n);
+}
+else
+{
+printf("%i is positive/n", n);
+}
+
+return (0);
+
+}
+{
+printf("%i is negatve\n", n);
 }
 
